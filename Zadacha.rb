c2ee98098f198bdc272d880c7a0c881ec064ff15
tarik <taraska100@ukr.net>
2022-11-14 22:35:13 +0200

    dopovnennya

--- Zadacha.rb
+++ Zadacha.rb
@@ -4,5 +4,19 @@ a = arr[rand(arr.size)]
 puts "Pershe randomne chuslo-#{a}"
 b = arr[rand(arr.size)]
 puts "Druge randomne chuslo-#{b}"
-c = a + b
-puts "Ix suma-#{c}"
+random_oper = ["+","-","*","/"]
+operat = random_oper[rand(random_oper.size)]
+puts "Randomna operatsia #{operat}"
+case operat
+when "+"
+    rezul_1=a+b
+when "-"
+    rezul_2=a-b
+when "*"
+    rezul_3=a*b
+when "/"
+    rezul_4=a/b
+end  
+puts"Vuraz zagalom: #{a}#{operat}#{b}"
+puts"Vvedit rezultat tsogo vurazy:"
+rezultat_korust=readline
